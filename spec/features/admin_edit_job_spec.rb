bf3ab44b8ae71f941589153e189a82192e9e21f6
juniorivn <juninhoivn@gmail.com>
2021-03-05 03:20:42 -0300

    Incluso novos testes em: Editar candidates/jobs

--- spec/features/admin_edit_job_spec.rb
+++ spec/features/admin_edit_job_spec.rb
@@ -54,4 +54,38 @@ feature 'Admin updates jobs' do
 
   end
 
+  scenario 'update attributes cannot be blank' do
+    collaborator = Collaborator.create!(email: 'ivan@email.com', password: '123456', cpf: '26701421687',
+                                        name: 'Ivan de Oliveira Junior')
+    login_as collaborator, scope: :collaborator
+    job = Job.create!(title: 'Analista Ruby', description: ' Trabalhará com desenolvimento de projeto em Ruby',
+                      salary: '2500', level: 'Junior',
+                      requirements: 'Certificação em Ruby', 
+                      deadline: '22/12/2033', total: '5', company_id: collaborator.company_id)
+
+    
+    visit root_path
+    click_on "Acesso empresa"
+    click_on "Vagas de emprego cadastradas"
+    click_on job.title
+    click_on "Editar vaga de emprego"
+
+    fill_in 'Título da vaga', with: ''
+    fill_in 'Descrição', with: ''
+    fill_in 'Faixa salarial', with: ''
+    select 'Pleno'
+    fill_in 'Requisitos', with: ''
+    fill_in 'Data limite', with: ''
+    fill_in 'Total de posições', with: ''
+    click_on 'Atualizar Vaga de Emprego'
+
+    expect(page).to have_content('Não foi possível editar vaga de emprego')
+    expect(page).to have_content('Título da vaga não pode ficar em branco')
+    expect(page).to have_content('Descrição não pode ficar em branco')
+    expect(page).to have_content('Faixa salarial não pode ficar em branco')
+    expect(page).to have_content('Data limite não pode ficar em branco')
+    expect(page).to have_content('Total de posições não pode ficar em branco')
+
+  end
+
 end
--- spec/features/candidate_edit_profile_spec.rb
+++ spec/features/candidate_edit_profile_spec.rb
@@ -43,4 +43,35 @@ feature 'Collaborator updates jobs' do
 
   end
 
+  scenario 'update attributes cannot be blank' do
+    candidate = Candidate.create!(email: 'ivan@email.com', password: '123456', cpf: '26701421687',
+                                  name: 'Ivan de Oliveira Junior', cell_phone: '98199837',
+                                  short_biography: 'Histórico Profisional', academic_training: 'Formado em SI')
+    login_as candidate, scope: :candidate
+  
+    visit root_path
+    click_on "Acesso candidato"
+    click_on "Editar perfil"
+
+    fill_in 'E-mail', with: ''
+    fill_in 'Nome', with: ''
+    fill_in 'CPF', with: ''
+    fill_in 'Celular', with: ''
+    fill_in 'Biografia resumida', with: ''
+    fill_in 'Formação academica', with: ''
+    fill_in 'Senha', with: ''
+    fill_in 'Confirme sua senha', with: ''
+    
+    click_on 'Atualizar'
+
+    expect(page).to have_content('Não foi possível editar vaga de emprego')
+    expect(page).to have_content('Nome não pode ficar em branco')
+    expect(page).to have_content('E-mail não pode ficar em branco')
+    expect(page).to have_content('CPF não pode ficar em branco')
+    expect(page).to have_content('Celular não pode ficar em branco')
+    expect(page).to have_content('Biografia resumida não pode ficar em branco')
+    expect(page).to have_content('Formação academica não pode ficar em branco')
+    expect(page).to have_content('Senha não pode ficar em branco')
+
+  end
 end
